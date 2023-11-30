d8a1be59dd9f00961d41aa35968d6666bc0d5444
baejb <bjb6478@naver.com>
2023-12-01 00:15:54 +0900

    랜딩페이지 / 로그인페이지 라우팅연결

--- mmmm/src/App.jsx
+++ mmmm/src/App.jsx
@@ -1,12 +1,17 @@
 import './App.css'
-
+import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
 function App() {
  
 
   return (
     <>
       <div>
-        hi
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
         </div>
     </>
   )
---
+++ mmmm/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            랜딩페이지
+        </div>
+    );
+};
+
+export default MainPage;
